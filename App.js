9e3c88b2b4779d6be3cfc8c4a4723405545591e4
ANA BEATRIZ BENITES GARDENAL <anab.gardenal@gmail.com>
2026-08-14 13:07:02 +0000

    Fim Exemplo2

--- App.js
+++ App.js
@@ -1,52 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-import { Entypo, EvilIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-
-import styles from './styles/style';
+import ListaDeSignos from './components/ListaDeSignos';
+import ListaDeSignosAuto from './components/ListaDeSignosAuto';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <View style={styles.card}>
-        <Text style={styles.titulo}>Entypo</Text>
-
-        <View style={styles.icones}>
-          <Entypo name="arrow-bold-down" size={30} color="pink" />
-          <Entypo name="arrow-bold-left" size={30} color="pink" />
-          <Entypo name="arrow-bold-right" size={30} color="pink" />
-          <Entypo name="chat" size={30} color="purple" />
-          <Entypo name="cloud" size={30} color="black" />
-        </View>
-      </View>
-
-      <View style={styles.card}>
-        <Text style={styles.titulo}>EvilIcons</Text>
-
-        <View style={styles.icones}>
-          <EvilIcons name="close" size={40} color="green" />
-          <EvilIcons name="location" size={40} color="darkblue" />
-          <EvilIcons name="calendar" size={40} color="pink" />
-          <EvilIcons name="star" size={40} color="red" />
-          <EvilIcons name="trash" size={40} color="gray" />
-          <EvilIcons name="search" size={40} color="gray" />
-        </View>
-      </View>
-
-      <View style={styles.card}>
-        <Text style={styles.titulo}>MaterialCommunityIcons</Text>
-
-        <View style={styles.icones}>
-          <MaterialCommunityIcons name="account" size={50} color="brown" />
-          <MaterialCommunityIcons name="clock-outline" size={50} color="purple" />
-          <MaterialCommunityIcons name="numeric-1-circle" size={50} color="green" />
-          <MaterialCommunityIcons name="reload" size={50} color="yellow" />
-          <MaterialCommunityIcons name="camera" size={50} color="dark" />
-          <MaterialCommunityIcons name="cart" size={50} color="darkblue" />
-        </View>
-      </View>
-
-    </View>
+    // Escolha qual componente você quer renderizar, ListaDeSignos ou ListaDeSignosAuto
+    // É só comentar o que não quer usar e descomentar o que quer usar
+    <ListaDeSignos />
+    // <ListaDeSignosAuto />
   );
 }
